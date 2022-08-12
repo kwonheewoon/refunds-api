55caa9ec3c6e3513e8c746b7c6af80c009730bf4
dybala <dybala.arg@dktechin.com>
2022-08-12 17:10:24 +0900

    Initial commit

---
+++ src/main/java/main/refundsapi/RefundsApiApplication.java
@@ -0,0 +1,13 @@
+package main.refundsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefundsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefundsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/main/refundsapi/RefundsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package main.refundsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefundsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
